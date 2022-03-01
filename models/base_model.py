5252dd291d4e9b844ded7b52e35a5044fbfbb236
AnniesTech <anita.rocha0505@gmail.com>
2022-03-01 17:23:52 -0500

    finished point 3 with base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 """ Class BaseModel"""
 
-import models
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-    """Class BaseModel that defines all common attributes/methods for other classes"""
-    def __init__(self, *args, **kwargs):
-            """Initialization of the base model"""
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+    """Class BaseModel that defines all common
+    attributes/methods for other classes"""
+    def __init__(self):
+        """Initialization of the base model"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """String representation of the BaseModel class"""
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                            self.__dict__)
+                                         self.__dict__)
 
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        
+        """ Returns a dictorionary of all keys and values of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = new_dict["created_at"].isoformat
+        new_dict["updated_at"] = new_dict["updated_at"].isoformat
+        return new_dict
